04d15620aa3b104deaeb5e31b42fe81c6d83cfe1
kebasaa <muellerjonathan@gmx.net>
2025-10-03 18:46:22 +0200

    Multiplexer compatibility

--- lib/ADS1115/ADS1115.cpp
+++ lib/ADS1115/ADS1115.cpp
@@ -10,14 +10,50 @@
 #include "ADS1X15.h"
 ADS1115 ads_board(0x48);
 
-ADS::ADS(void){
-  // ADS
+ADS::ADS(
+#if I2C_MULTI
+  MULTI* mux, uint8_t bus
+#endif
+) {
+#if I2C_MULTI
+  _mux = mux;
+  _mux_bus = bus;
+#endif
 }
 
+#if I2C_MULTI
+void ADS::setMultiplexer(MULTI* mux, uint8_t bus) {
+  _mux = mux;
+  _mux_bus = bus;
+}
+#endif
+
+// Simple RAII guard to enable/disable a mux channel while in scope
+#if I2C_MULTI
+struct BusGuard {
+  MULTI* mux;
+  uint8_t bus;
+  BusGuard(MULTI* m, uint8_t b) : mux(m), bus(b) {
+    if(mux) mux->enableBus(bus);
+  }
+  ~BusGuard() {
+    if(mux) mux->disableBus(bus);
+  }
+};
+#endif
+
 bool ADS::init(void){
+#if I2C_MULTI
+  BusGuard guard(_mux, _mux_bus);
+#endif
+
   if(!ads_board.begin()){
+    error_status = 1; // Initialisation failed
+    sensorPresent = false;
     return(false);
   } else {
+    error_status = 0;
+    sensorPresent = true;
     ads_board.setGain(16); // Gain 16: ±0.256V
     ads_board.readADC(1);
     return(true);
@@ -25,6 +61,13 @@ bool ADS::init(void){
 }
 
 double ADS::read_mV(int port, int gain){
+  if(sensorPresent == false){
+    return(float(NAN));
+  }
+#if I2C_MULTI
+  BusGuard guard(_mux, _mux_bus);
+#endif
+
   int16_t val = 0;
   // Gain 16: ±0.256V
   ads_board.setGain(gain);
@@ -32,13 +75,11 @@ double ADS::read_mV(int port, int gain){
 	// Read data
 	val = ads_board.readADC(port);
 	double f = ads_board.toVoltage();  // voltage factor
-  //Serial.print(val); Serial.print("\t");
-  //Serial.print(f,6); Serial.print("\t");
-  //Serial.println(mV,6);
 
   // Convert to mV
   double mV = val * f * 1000;
-  // Negative values should be made 0
+  error_status = (mV < 0) ? 2 : 0; // Make error_status 2 when values were <0, otherwise 0
+  // Force negative values to be 0
   mV = (mV <= 0) ? 0.0 : mV; // If smaller than 0, make the value 0
 	
 	return(mV);
@@ -51,6 +92,10 @@ double ADS::read_val(int port, int gain, int conversion){
 }
 
 void  ADS::nothing(void){
+#if I2C_MULTI
+  BusGuard guard(_mux, _mux_bus);
+#endif
+
 	// Nothing
   Serial.print("Mode (0=cont): "); Serial.println(ads_board.getMode());
 }
--- lib/ADS1115/ADS1115.h
+++ lib/ADS1115/ADS1115.h
@@ -2,16 +2,27 @@
  * ADS1115 Analog-to-digital converter
  *
  */
+#pragma once
 
 #ifndef ADS1115h_h
 #define ADS1115h_h
 
 #include "Arduino.h"
 
+#include "config.h"
+#if I2C_MULTI
+#include "TCA9548_multiplexer.h"
+#endif
+
 class ADS {
   public:
     // main Class
+  #if I2C_MULTI
+    ADS(MULTI* mux = nullptr, uint8_t bus = 0);
+    void setMultiplexer(MULTI* mux, uint8_t bus);
+  #else
     ADS(void);
+  #endif
   
     // Functions
     //----------
@@ -21,8 +32,15 @@ class ADS {
     void   nothing(void);
 
   private:
-    int error_status = 0;
-    int i2c_bus_id = 1;
+    bool sensorPresent = false;
+    int  error_status = 0;
+    int  i2c_bus_id = 1;
+
+  
+  #if I2C_MULTI
+    MULTI* _mux = nullptr;
+    uint8_t _mux_bus = 0;
+  #endif
 };
 
 #endif
